0c11509dbe932422b0ec7742201458903a555941
Forkeh <forkeh@gmail.com>
2024-02-13 22:43:08 +0100

    added CourseRepository

---
+++ src/main/java/eduhogwarts/hogwartsadmin/repositories/CourseRepository.java
@@ -0,0 +1,7 @@
+package eduhogwarts.hogwartsadmin.repositories;
+
+import eduhogwarts.hogwartsadmin.models.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Integer> {
+}
